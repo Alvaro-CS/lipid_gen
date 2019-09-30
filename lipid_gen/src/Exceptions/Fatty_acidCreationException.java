bd4e1666629cc2442e42b1d4325162c70a0fd8f9
Alvaro-CS <a.cascajero@usp.ceu.es>
2019-09-30 21:02:54 +0200

    Intento de Excepción

---
+++ lipid_gen/src/Exceptions/Fatty_acidCreationException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class Fatty_acidCreationException extends Exception {//TODO serializable?
+public Fatty_acidCreationException(String errorMessage) {
+	super(errorMessage);
+}
+}
--- lipid_gen/src/model/Fatty_acid.java
+++ lipid_gen/src/model/Fatty_acid.java
@@ -1,13 +1,14 @@
 package model;
 
+import Exceptions.Fatty_acidCreationException;
 
 public class Fatty_acid {
 /*int ncarbon;
 int double_bonds; Is it necessary?*/
 Formula formula;
-public Fatty_acid(int C,int double_bonds) {//TODO Excepcion doublebonds-c, limite carbonos
+public Fatty_acid(int C,int double_bonds) throws Fatty_acidCreationException {//TODO Excepcion doublebonds-c, limite carbonos
 	
-	//if(double_bonds > C - 1)throw Exception;
+	if(double_bonds > C - 1)throw new Fatty_acidCreationException("Double bonds of the molecule must be at least lower than the number of Carbon atoms-1.");
 	this.formula=new Formula(C, double_bonds);
 }
 
